8f823f96506defcc55bd1bc223fa7eb95a517371
kalmarek <kalmar@amu.edu.pl>
2019-07-09 17:03:16 +0200

    remove comments from script

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -12,22 +12,11 @@ sources = [
 
 ]
 
-# Adapted from builder by kalmarek (https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/313)
 # Bash recipe for building across all platforms 
 script = raw"""
 cd $WORKSPACE/srcdir
 cd scs-2.0.2/
-# Mac OS and Windows do not have openmp by default
-#if [ $target = "x86_64-apple-darwin14" ] || [ $target = "x86_64-w64-mingw32" ] || [ $target = "i686-w64-mingw32" ]; then
-#    flags="DLONG=1"
-#else
-#    flags="DLONG=1 USE_OPENMP=1"
-#fi
-# GOMP 4.0 would be required for OPENMP, which makes travis fail because it only has gcc version 4.8.4 
 flags="DLONG=1 USE_OPENMP=0"
-#if [ $target = "x86_64-apple-darwin14" ]; then 
-#    install_name_tool -id libopenblas64_.dylib ${prefix}/lib/libopenblas64_.0.3.0.dev.dylib
-#fi
 blasldflags="-L${prefix}/lib"
 if [[ ${nbits} == 32 ]]; then     blasldflags="${blasldflags} -lopenblas"; else     flags="${flags} BLAS64=1 BLASSUFFIX=_64_";     blasldflags="${blasldflags} -lopenblas64_"; fi
 make BLASLDFLAGS="${blasldflags}" ${flags} out/libscsdir.${dlext}
